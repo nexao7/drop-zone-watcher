b4c2f1deaa2baafdcecc168689df64756874f9b1
Cyril TAVIAN <cyril.tavian@octo.com>
2024-06-03 11:53:42 +0200

    feat: init with files

---
+++ js/map.js
@@ -0,0 +1,18 @@
+var map = L.map('mapid').setView([46.603354, 1.888334], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+fetch('dropzones.yaml')
+    .then(response => response.text())
+    .then(data => {
+        var dropZones = jsyaml.load(data).dropZones;
+
+        dropZones.forEach(function(dz) {
+            L.marker([dz.lat, dz.lng]).addTo(map)
+                .bindPopup(dz.name)
+                .openPopup();
+        });
+    })
+    .catch(error => console.log(error));
